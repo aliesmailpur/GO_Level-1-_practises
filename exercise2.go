1191441b57183145fba6c0e16f499cd1108ab820
aliesmailpur <77744482+aliesmailpur@users.noreply.github.com>
2022-10-12 20:52:22 +0330

    Add files via upload

---
+++ exercise2.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// use var to DECLARE three variables. the variiables shoud have package level scope.
+// Do not assign VALUES to the variables. use the following 
+//Identifiers for the variables and make sure the variables are of the following TYPE(
+// meaning they can store Values of that type)
+// then pring out the values for each identifier
+
+
+var x int
+var y string
+var z bool
+
+func main() {
+      fmt.Println(x)
+	  fmt.Println(y)
+	  fmt.Println(z)
+	
+
+}
