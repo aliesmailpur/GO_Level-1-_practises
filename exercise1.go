3ef41ffeb702be4a2369b9cc0babdc8d52fa3c10
aliesmailpur <77744482+aliesmailpur@users.noreply.github.com>
2022-10-12 20:36:32 +0330

    Add files via upload

---
+++ exercise1.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+// exercise number 1 
+
+// using the short declaratiion operator, ASSIGN these VALUES to VARIABLES with the IDENTIFIERS
+// "x" "y" and "z"
+// a = 42
+// b = "james bond"
+// c = true 
+
+// now print the values stored in those variable using
+// a. A single print statement
+// multiple print statement
+
+
+func main() {
+      
+	x := 42
+	y := "james bond"
+	z := true
+
+    fmt.Println(x , y ,z)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+
+}
